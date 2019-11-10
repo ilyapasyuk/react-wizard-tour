9f0a481c2f8ecc7ece3ef29f4d868fc949d98f46
ilyapasyuk <pasyuk.com@gmail.com>
2019-11-11 00:10:33 +0300

    use rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,22 @@
-import {uglify} from 'rollup-plugin-uglify'
+import { terser } from "rollup-plugin-terser";
 import babel from 'rollup-plugin-babel'
 
 const config = {
     input: 'src/index.js',
     external: ['react'],
-    output: {
-        format: 'umd',
-        name: 'wizard',
-        globals: {
-            react: 'React',
-        },
-    },
+    output: [
+        { file: 'lib.js', format: 'cjs' },
+        { file: 'lib.min.js', format: 'cjs' },
+        { file: 'lib.esm.js', format: 'es' },
+    ],
     plugins: [
         babel({
             exclude: 'node_modules/**',
         }),
-        uglify(),
+        terser({
+            include: [/^.+\.min\.js$/, '*esm*'],
+            exclude: [ 'some*' ]
+        })
     ],
 }
 
